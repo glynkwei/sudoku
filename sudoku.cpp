26bb27d2c7b53fa7a1a29d4b4fe650493be33f00
Glyn Wei <glynk.wei@gmail.com>
2018-02-20 00:36:33 -0500

    added extern to prevent name wrangling

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,12 +1,14 @@
 #include "sudoku.hpp"
-int wrapper_sudoku_solver(int block_row_count, int block_col_count, int* buffer_in, int* buffer_out) 
-{
-	auto N = block_row_count * block_col_count;
-	sudoku::Solver solver(block_row_count, block_col_count);
-	auto analysis = solver.solve(buffer_in, buffer_out);
-	if (analysis.solutions_found == 1)
-	{		
-		return 0;
+extern "C" {
+	int wrapper_sudoku_solver(int block_row_count, int block_col_count, int* buffer_in, int* buffer_out)
+	{
+		auto N = block_row_count * block_col_count;
+		sudoku::Solver solver(block_row_count, block_col_count);
+		auto analysis = solver.solve(buffer_in, buffer_out);
+		if (analysis.solutions_found == 1)
+		{
+			return 0;
+		}
+		return 1;
 	}
-	return 1;
 }
